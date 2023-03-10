645a97b14fe3d9e3314a231f71e7a9d332b90c56
Space4578919 <122700047+Space4578919@users.noreply.github.com>
2023-03-10 23:53:07 +0100

    Create MainViewModel.java

---
+++ app/src/main/java/cz/android/games/apps/lilith/samuraiwarrior/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package cz.android.games.apps.lilith.samuraiwarrior.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
